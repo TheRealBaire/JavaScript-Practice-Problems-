a9ebc23c67ef9e02194ace880ff855f8c8bca053
Akinyele Mayowa Temitope <90946444+TheRealBaire@users.noreply.github.com>
2024-06-09 07:47:24 +0100

    Create Functions.js
    Signed-off-by: Akinyele Mayowa Temitope <90946444+TheRealBaire@users.noreply.github.com>

---
+++ Functions.js
@@ -0,0 +1,5 @@
+/* This file is a compilation of practice problems solved using functions.
+Function Expression and Function Declaration are the practices employes in thus compilation. 
+The functions here would be imported and used  later on in other problems. */
+
+console.log("Hi!")
