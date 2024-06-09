a47c40ccfaa591bdf1b8f4be8d9bf1cbde801e37
Akinyele Mayowa Temitope <90946444+TheRealBaire@users.noreply.github.com>
2024-06-09 08:13:13 +0100

    Update Functions.js
    Signed-off-by: Akinyele Mayowa Temitope <90946444+TheRealBaire@users.noreply.github.com>

--- Functions.js
+++ Functions.js
@@ -2,4 +2,10 @@
 Function Expression and Function Declaration are the practices employes in thus compilation. 
 The functions here would be imported and used  later on in other problems. */
 
-console.log("Hi!")
+// Which is the biggest number of these arguments
+function minMax(x, y) {
+  if (x > y) 
+    return `${x} is greater`
+  else 
+    return `${y} is greater`
+}
