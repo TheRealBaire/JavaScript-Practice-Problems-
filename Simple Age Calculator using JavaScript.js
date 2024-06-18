4954da7857d6748fdb64aa75018d88e16a87a03c
Akinyele Mayowa Temitope <90946444+TheRealBaire@users.noreply.github.com>
2024-06-18 19:23:41 +0100

    Simple Age Calculator using JavaScript.js
    Signed-off-by: Akinyele Mayowa Temitope <90946444+TheRealBaire@users.noreply.github.com>

---
+++ Simple Age Calculator using JavaScript.js
@@ -0,0 +1,19 @@
+/*
+Robaire Thias
+*/
+function ageCalc(year, current){
+  if ( year >= Number(current) || year <= 1824){
+    return "Erroneous"
+  }
+  else 
+    return Number(current)- Number(year)
+}
+
+let birthYear= prompt("Year Of Birth", "");
+let newYear = prompt ("Current Year", "")
+result = ageCalc(birthYear, newYear);
+if (isNaN(result) == false){
+  window.alert("You are" + " " +result+ " years old!")
+  }
+else 
+  window.alert("An error occured")
